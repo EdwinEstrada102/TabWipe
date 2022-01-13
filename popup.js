fc85b7fb865202b7a5c8bb0e19f099de030de56c
EdwinEstrada102 <87724218+EdwinEstrada102@users.noreply.github.com>
2022-01-12 17:12:42 -0800

    Add files via upload
    Log 3: 
    -Fixed a bug where if you're on the Saved Tabs list, you press on the Wipe Tabs button, and you switch to the Unsaved Tabs list, you will still see the list of tabs that will be closed
    -Fixed a bug where if you're on the Unsaved Tabs list and you press the Wipe Tabs button, you still see the items on the Unsaved Tabs list.

--- popup.js
+++ popup.js
@@ -6,15 +6,26 @@ let body = document.body;
 let titleSpan = document.getElementById("list");
 let currentWindow = chrome.windows.WINDOW_ID_CURRENT
 let listDiv = document.getElementById("listDiv");
+let unsaved = [];
 let saved = [];
-chrome.storage.sync.get("saved", (obj) => saved = obj["saved"]);
-const unsaved = [];
+let tabList = [];
+let onUnsaved = false;
+chrome.storage.local.get("saved", (obj) => { saved = obj["saved"];
 
 chrome.tabs.query(
   {windowId: currentWindow},
   (tabs) => {
-    for(let i=0; i<tabs.length; i++){
-      if(saved.find( elements => elements.id=="checkbox"+i)==undefined){
+    if(saved!=undefined){
+      for(let i=0; i<tabs.length; i++){
+        if(saved.find(element => element.url==tabs[i].url)==undefined){
+          unsaved.push({"id": "checkbox"+i, "url": tabs[i].url, "tabId": tabs[i].id});
+          tabList.push(tabs[i]);
+        }
+      }
+    }
+    else{
+      saved = [];
+      for(let i=0; i<tabs.length; i++){
         unsaved.push({"id": "checkbox"+i, "url": tabs[i].url, "tabId": tabs[i].id});
       }
     }
@@ -22,16 +33,26 @@ chrome.tabs.query(
 );
 
 tabWipeButton.addEventListener("click", () => {
-    for(let i=0; i<unsaved.length; i++){
-      chrome.tabs.remove(unsaved[i].tabId)
+    if(onUnsaved){
+      for(let i=0; i<unsaved.length; i++){
+        document.getElementById(unsaved[i].id).parentNode.remove();
+      }
     }
-    listDiv.innerHTML=""
+    for(let i=0; i<tabList.length; i++){
+      if(saved.find(element => element.url == tabList[i].url) == undefined){
+        chrome.tabs.remove(tabList[i].id);
+        tabList.splice(i, 1);
+        i--
+      }
+    }
+    unsaved = [];
   });
 
   savedButton.addEventListener("click", async () => {
+    onUnsaved = false;
     listDiv.innerHTML=""
     titleSpan.innerText="Saved Tabs"
-    chrome.storage.sync.get("saved", (obj) => {saved = obj["saved"];
+    // chrome.storage.local.get("saved", (obj) => { saved = obj["saved"];
     saved.sort(function(a, b) {if(a.id<b.id){return -1;}if(a.id>b.id){return 1;}});
         for(let i=0; i<saved.length; i++){
 			let newP = document.createElement("p");
@@ -44,12 +65,12 @@ tabWipeButton.addEventListener("click", () => {
 			addInput.setAttribute("type", "checkbox");
       addInput.setAttribute("id", saved[i].id);
       addInput.setAttribute("checked", true);
-      document.getElementById(saved[i].id).addEventListener("change", (event) => {let deleteDiv = document.getElementById(event.currentTarget.id).parentNode; deleteDiv.remove(); unsaved.push({"id": event.currentTarget.id, "url":saved.find(element => element.id == event.currentTarget.id).url, "tabId":saved.find(element => element.id == event.currentTarget.id).tabId}); saved.splice(saved.indexOf(saved.find(element => element.id == event.currentTarget.id)), 1); chrome.storage.sync.set({"saved": saved})});
+      document.getElementById(saved[i].id).addEventListener("change", (event) => {let deleteDiv = document.getElementById(event.currentTarget.id).parentNode; deleteDiv.remove(); unsaved.push({"id": event.currentTarget.id, "url":saved.find(element => element.id == event.currentTarget.id).url, "tabId":saved.find(element => element.id == event.currentTarget.id).tabId}); saved.splice(saved.indexOf(saved.find(element => element.id == event.currentTarget.id)), 1); chrome.storage.local.set({"saved": saved})});
             addP.innerText=saved[i].url;
-        }});
-  });
+        }});})
 
   unsavedButton.addEventListener("click", async () => {
+    onUnsaved = true;
     listDiv.innerHTML=""
     titleSpan.innerText="Unsaved Tabs"
     unsaved.sort(function(a, b) {if(a.id<b.id){return -1;}if(a.id>b.id){return 1;}});
@@ -63,7 +84,9 @@ tabWipeButton.addEventListener("click", () => {
 			addDiv.setAttribute("class", "listItem");
 			addInput.setAttribute("type", "checkbox");
       addInput.setAttribute("id", unsaved[i].id);
-      document.getElementById(unsaved[i].id).addEventListener("change", (event) => {let deleteDiv = document.getElementById(event.currentTarget.id).parentNode; deleteDiv.remove(); saved.push({"id": event.currentTarget.id, "url":unsaved.find(element => element.id == event.currentTarget.id).url, "tabId":unsaved.find(element => element.id == event.currentTarget.id).tabId}); unsaved.splice(unsaved.indexOf(unsaved.find(element => element.id == event.currentTarget.id)), 1); chrome.storage.sync.set({"saved": saved})});
+      document.getElementById(unsaved[i].id).addEventListener("change", (event) => {let deleteDiv = document.getElementById(event.currentTarget.id).parentNode; deleteDiv.remove(); saved.push({"id": event.currentTarget.id, "url":unsaved.find(element => element.id == event.currentTarget.id).url, "tabId":unsaved.find(element => element.id == event.currentTarget.id).tabId}); unsaved.splice(unsaved.indexOf(unsaved.find(element => element.id == event.currentTarget.id)), 1); chrome.storage.local.set({"saved": saved})});
             addP.innerText=unsaved[i].url;
         }
   })
+
+  // chrome.storage.sync.get(["newURL", "tabId"], (obj) => {if(saved.find(element => element.tabId == obj["tabId"].tabId)!=undefined){saved[saved.indexOf(saved.find(element => element.tabId == obj["tabId"].tabId))].url = obj["newURL"].url}})
